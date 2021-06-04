a8977d53431fcd4e9a1a05912639ebeea5b0dbb4
Shradha1824 <shradhabj1824@gmail.com>
2021-06-03 15:49:14 -1200

    Added file to sort data in steam

--- AddressBook.java
+++ AddressBook.java
@@ -5,11 +5,15 @@ import java.util.stream.Stream;
 
 public class AddressBook {
 	public static void main(String[] args)
-    {
+     	{
   
         // Creating a Stream of Strings
         Stream<String> stream = Stream.of("Anusha", "Vaishnavi",
         "Shakti", "Geet","Ravi");
+
+	List<String> stringlist = Arrays.asList("Aahan", "Shashi",
+		"preeti", "Ganesh", "Kundan", "Suresh");
+		
   
         // Using Stream filter(Predicate predicate)
         // to get a stream consisting of the
@@ -17,8 +21,12 @@ public class AddressBook {
         stream.filter(str -> str.startsWith("S"))
             .forEach(System.out::println);
    
-	 stream.filter(str -> str.length() > 3)
-        .forEach(System.out::println);
+	stream.filter(str -> str.length() > 3)
+            .forEach(System.out::println);
      
-    }
+	//stringlist.stream().sorted().forEach(System.out::println);     
+	List<String> sortedList = stringlist.stream().sorted().collect(Collectors.toList());  
+	    sortedList.forEach(System.out::println);
+	    
+      	}
 }
